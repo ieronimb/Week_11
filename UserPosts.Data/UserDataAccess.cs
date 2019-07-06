8578a63b88cc4ab95be6eda18f9e58ffb4571fe8
Birnat Ieronim <Ieronimb@outlook.com>
2019-07-06 11:55:59 +0300

    Add project files.

---
+++ UserPosts.Data/UserDataAccess.cs
@@ -0,0 +1,13 @@
+﻿using UserPosts.Domain;
+using UserPosts.Services;
+
+namespace UserPosts.Data
+{
+    public class UserDataAccess : BaseDataAccess<User>, IUserRepository
+    {
+        protected override string GetFile()
+        {
+            return @"D:\projects\wantsome\week11\UserPosts\UserPosts.Data\Files\users.json";
+        }
+    }
+}
---
+++ UserPosts.Services.Tests/UserServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace UserPosts.Services.Tests
+{
+    [TestFixture]
+    public class UserServiceTests
+    {
+        UserService sut;
+
+        [Test]
+        public void Test()
+        {
+            IUserRepository userRepository = new UserDummyRepo();
+            IPostRepository postRepository = new PostDummyRepo();
+
+            sut = new UserService(userRepository, postRepository);
+
+            var expected = new UserActiveRespose()
+            {
+                Email = "andrei@wtf.com",
+                Status = UserPostsStatus.Inactive
+            };
+
+            var actual = sut.GetUserActiveRespose(1);
+
+            Assert.AreEqual(expected.Email, actual.Email);
+            Assert.AreEqual(expected.Status, actual.Status);
+        }
+    }
+}
